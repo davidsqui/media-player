2846b47c88108ef7c76f5f655388f868313807e1
DMSANCHEZQ <davidsqr6@gmail.com>
2021-06-20 11:30:17 -0500

    test

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+const video = document.querySelector('video');
+const button = document.querySelector('button');
+
+function MediaPlayer(config) {
+    this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function () {
+    this.media.play();
+}
+
+MediaPlayer.prototype.pause = function () {
+    this.media.pause();
+}
+
+const player = new MediaPlayer({ el: video });
+const toggle = () => {
+    video.paused ? player.play() : player.pause();
+}
+button.onclick = () => toggle();
