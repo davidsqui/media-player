9fe7f91a98561be837d006cabd1f5cfcb2c74540
DMSANCHEZQ <davidsqr6@gmail.com>
2021-06-26 01:24:16 -0500

    refactor: Refactorización del plugin autoplay

--- src/assets/plugins/autoplay.js
+++ src/assets/plugins/autoplay.js
@@ -1,11 +1,11 @@
-function AutoPlay() { }
+class AutoPlay {
 
-AutoPlay.prototype.run = function (player) {
-    if (!player.muted) {
-        player.muted = true;
-    }
-
-    player.play();
-};
+    run(player) {
+        if (!player.muted) {
+            player.muted = true;
+        }
+        player.play();
+    };
+}
 
 export default AutoPlay;
