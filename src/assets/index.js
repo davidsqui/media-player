5f5634b03b93711df579067039d1308e85fb2391
DMSANCHEZQ <davidsqr6@gmail.com>
2021-06-26 01:21:36 -0500

    feat: Implementación del plugin autopause

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,12 @@
 import MediaPlayer from './media-player.js';
 import AutoPlay from './plugins/autoplay.js'
+import AutoPause from './plugins/autopause.js'
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({
+    el: video,
+    plugins: [new AutoPlay(), new AutoPause()]
+});
 const playButton = document.querySelector('#playButton');
 const muteButton = document.querySelector('#muteButton');
 
